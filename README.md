11c2b8932cba2f75aa9261df57920a9b787cb35b
juanrios15 <juankrios15@gmail.com>
2023-03-08 15:42:26 -0500

    set access token, databases and pages requests

--- README.md
+++ README.md
@@ -1,5 +1,96 @@
-![](https://img.shields.io/badge/version-0.1.0-success) ![](https://img.shields.io/badge/Python-3.8%20|%203.9%20|%203.10%20|%203.11-4B8BBE?logo=python&logoColor=white)
 # notion-python
-
+![](https://img.shields.io/badge/version-0.1.0-success) ![](https://img.shields.io/badge/Python-3.8%20|%203.9%20|%203.10%20|%203.11-4B8BBE?logo=python&logoColor=white)
+  
 *notion-python* is an API wrapper for Notion, written in Python.  
 This library uses Oauth2 for authentication.
+## Installing
+```
+pip install notion-python
+```
+## Usage
+If you don't have an access token:
+```
+from notion.client import Client
+client = Client(client_id, client_secret, redirect_uri)
+```
+To obtain and set an access token, follow this instructions:
+1. **Get authorization URL**
+```
+url = client.authorization_url()
+```
+2. **Get access token using code**
+```
+response = client.get_access_token(code)
+```
+3. **Set access token**
+```
+client.set_token(access_token)
+```  
+Check more information about Notion Oauth: https://developers.notion.com/docs/authorization
+If you already have an access token:
+```
+from notion.client import Client
+client = Client(access_token=access_token)
+```
+### User
+#### - Get Current User
+```
+user = client.get_current_user()
+```
+### Databases and Pages
+#### - List all objects
+```
+# object_type options are: page or database
+# page_size: max 100 
+# start_cursor: pagination variable, get this value from previous page 'next_cursor' parameter.
+
+databases_list = client.list_objects("database", page_size=5)
+```
+#### - Get database
+```
+database = client.get_database(database_id)
+```
+#### - Query database pages
+How to build a filter object: https://developers.notion.com/reference/post-database-query-filter  
+How to build a sort object: https://developers.notion.com/reference/post-database-query-sort
+```
+pages = client.query_database_pages(
+    database_id, 
+    filters: dict = None, 
+    sorts: dict = None, 
+    start_cursor=None, 
+    page_size=None
+)
+```
+#### - Create page
+```
+properties_example = {
+    "Project name": {
+        "title": [
+            {
+                "text": {
+                    "content": "First project built with this library"
+                    }
+            }
+        ]
+    },
+}
+cover_url = "https://upload.wikimedia.org/wikipedia/commons/1/1/example.jpg"
+page = client.create_page(database_id, properties_example, cover_url=cover_url)
+```
+#### - Update page
+```
+properties_example = {
+    "Project name": {
+        "title": [
+            {
+                "text": {
+                    "content": "Project field modified"
+                    }
+            }
+        ]
+    },
+}
+page = client.update_page(page_id, properties_example, cover_url=None)
+```
+How to build a properties object: https://developers.notion.com/reference/page-property-values
--- notion/client.py
+++ notion/client.py
@@ -1,3 +1,6 @@
+import json
+import base64
+
 import requests
 from urllib.parse import urlencode
 
@@ -7,7 +10,7 @@ from notion.exceptions import UnauthorizedError, WrongFormatInputError, Contacts
 class Client(object):
     URL = "https://api.notion.com/v1/"
     AUTH_ENDPOINT = "oauth/authorize?"
-    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    headers = {"Content-Type": "application/json", "Accept": "application/json", "Notion-Version": "2022-06-28"}
 
     def __init__(self, client_id=None, client_secret=None, redirect_uri=None, access_token=None):
         self.CLIENT_ID = client_id
@@ -15,6 +18,8 @@ class Client(object):
         self.REDIRECT_URI = redirect_uri
         if access_token is not None:
             pass
+        if client_id and client_secret:
+            self.CREDENTIALS = base64.b64encode(f"{self.CLIENT_ID}:{self.CLIENT_SECRET}".encode()).decode()
 
     def authorization_url(self, state=None):
         params = {
@@ -26,6 +31,61 @@ class Client(object):
         if state:
             params["state"] = state
         return self.URL + self.AUTH_ENDPOINT + urlencode(params)
+
+    def get_access_token(self, code):
+        body = {"grant_type": "authorization_code", "code": code, "redirect_uri": self.REDIRECT_URI}
+        headers = {"Authorization": f"Basic {self.CREDENTIALS}"}
+        return self.post("oauth/token", data=json.dumps(body), headers=headers)
+
+    def set_token(self, access_token):
+        self.headers.update(Authorization=f"Bearer {access_token}")
+
+    def get_current_user(self):
+        return self.get("users/me")
+
+    def list_objects(self, object_type, page_size=None, start_cursor=None):
+        """
+        object_type options are: page or database
+        page_size: max 100 
+        start_cursor: pagination variable, get this value from previous page 'next_cursor' parameter.
+        """
+        body = {"filter": {"value": object_type, "property": "object"}}
+        if page_size:
+            body["page_size"] = page_size
+        if start_cursor:
+            body["start_cursor"] = start_cursor
+        return self.post("search", data=json.dumps(body))
+
+    def get_database(self, database_id):
+        return self.get(f"databases/{database_id}")
+
+    def query_database_pages(
+        self, database_id, filters: dict = None, sorts: dict = None, start_cursor=None, page_size=None
+    ):
+        body = {}
+        if filters:
+            body["filter"] = filters
+        if sorts:
+            body["sort"] = sorts
+        if start_cursor:
+            body["start_cursor"] = start_cursor
+        if page_size:
+            body["page_size"] = page_size
+
+        return self.post(f"databases/{database_id}/query", data=json.dumps(body))
+
+    def create_page(self, database_id, properties, cover_url=None):
+        body = {"parent": {"database_id": database_id}, "properties": properties}
+        if cover_url:
+            body["cover"] = {"external": {"url": cover_url}}
+        return self.post("pages", data=json.dumps(body))
+
+    def update_page(self, page_id, properties, cover_url=None):
+        body = {"properties": properties}
+        if cover_url:
+            body["cover"] = {"external": {"url": cover_url}}
+        
+        return self.patch(f"pages/{page_id}", data=json.dumps(body))
 
     def get(self, endpoint, **kwargs):
         response = self.request("GET", endpoint, **kwargs)
